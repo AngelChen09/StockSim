4ea06cb628cfd3d6117d294b7f832cd11d85967b
AngelChen09 <110699188+AngelChen09@users.noreply.github.com>
2024-11-23 23:52:08 -0500

    chores: cleaned up code

--- src/main/java/utility/MarketTracker.java
+++ src/main/java/utility/MarketTracker.java
@@ -6,7 +6,6 @@ import utility.exceptions.RateLimitExceededException;
 import view.ViewManager;
 import view.view_events.UpdateStockEvent;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
